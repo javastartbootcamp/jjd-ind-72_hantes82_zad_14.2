4a0e779f8557044c0f187e67330bf4154509683c
Konrad Putelbergier <Konrad.Putelbergier@gmail.com>
2023-10-22 14:37:24 +0200

    Zrobione zadanie 14.2

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,52 @@
 package pl.javastart.task;
 
-public class Main {
+import java.util.*;
 
+public class Main {
     public static void main(String[] args) {
 
+        String filePath = "src/main/java/resources/vehicles.csv";
+        VehiclesWorkshop vehiclesWorkshop = new VehiclesWorkshop(filePath);
+        Scanner scan = new Scanner(System.in);
+        boolean continueProcess = false;
+        do {
+            int number = getNumber(scan);
+            switch (number) {
+                case 0 -> {
+                    continueProcess = false;
+                    if (!vehiclesWorkshop.getVehicles().isEmpty()) {
+                        FileOperations.writeQueueToFile(filePath, vehiclesWorkshop.getVehicles());
+                    }
+                    System.out.println("Closing the app...");
+                }
+                case 1 -> {
+                    vehiclesWorkshop.addNewVehicle();
+                    continueProcess = true;
+                }
+                case 2 -> {
+                    if (!vehiclesWorkshop.getVehicles().isEmpty()) {
+                        Vehicle vehicle = vehiclesWorkshop.getVehicles().poll();
+                        System.out.println(vehicle.toString());
+                    } else {
+                        System.out.println("There are no cars in the queue");
+                    }
+                    continueProcess = true;
+                }
+                default -> {
+                    System.out.println("Out of range");
+                    continueProcess = true;
+                }
+            }
+        } while (continueProcess);
+    }
+
+    private static int getNumber(Scanner scan) {
+        int number;
+        do {
+            System.out.println("Choose the option:\n0. Exit program\n1. Add a new vehicle\n2. Load next vehicle\n");
+            number = scan.nextInt();
+            scan.nextLine();
+        } while (number < 0 || number > 2);
+        return number;
     }
 }
---
+++ src/main/java/pl/javastart/task/Vehicle.java
@@ -0,0 +1,56 @@
+package pl.javastart.task;
+
+public class Vehicle {
+
+    private String type;
+    private String brand;
+    private String model;
+    private int prodYear;
+    private long mileage;
+    private String vin;
+
+    public Vehicle(String type, String brand, String model, int prodYear, long mileage, String vin) {
+        this.type = type;
+        this.brand = brand;
+        this.model = model;
+        this.prodYear = prodYear;
+        this.mileage = mileage;
+        this.vin = vin;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getProdYear() {
+        return prodYear;
+    }
+
+    public long getMileage() {
+        return mileage;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "type='" + type + '\'' +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", prodYear=" + prodYear +
+                ", mileage=" + mileage +
+                ", vin='" + vin + '\'' +
+                '}';
+    }
+}
